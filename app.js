cb65bbc787ccf1e96db1f3f1192aa02b989d33ec
farleybob <farleybob17@yahoo.com>
2021-03-06 05:56:12 -0800

    add link to mongodb database

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://admin-bob:test@123@cluster0.rkgce.mongodb.net/todolistDB", { useNewUrlParser: true });
 
 const postSchema = {
   title: String,
